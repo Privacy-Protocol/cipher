a537482b755c7347d0538d299c5e03984b7d92d1
obah <olusojiobah@gmail.com>
2026-03-26 11:05:26 +0100

    chore: deployed contracts to sepolia

---
+++ cipher-contracts/lib/deployments.ts
@@ -0,0 +1,7 @@
+export const deployments = {
+  "ETHEREUM-SEPOLIA": {
+    PrivateDaoAdapter: "0x8274C53d82C5874455E67F80603F2792C9757cBE",
+    HonkVerifier: "0x76A2d7a3F4927AaB90d2247eA541c305Ee8615F1",
+    ZKTranscriptLib: "0x20A1Ad46eCAFb29ae97e20D28736904b060C7Dc9",
+  },
+};
